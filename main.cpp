fe3d4e2aa05ce063eda28238eb22e2a624939f6d
raznan <muhammadraznan@satpaminternet.com>
2021-07-23 16:52:11 +0700

    Added NODE_DATA class to store DECISION_NODE metadata

--- main.cpp
+++ main.cpp
@@ -12,6 +12,9 @@ namespace GML {
   enum COND {EQ, NEQ, LT, LTE, GT, GTE};
   enum MODE {BINARY, RANKED, MULTIPLE};
 
+  using CLASS_COUNT = std::unordered_map<std::string, size_t>; // All classifier total amount inside a TDATA
+  using PRES_CONFIDENCE = std::unordered_map<std::string, std::string>; // Prediction Result Confidence
+
   template<typename T>
     class DATA : public std::vector<T> {
       public:
@@ -55,12 +58,12 @@ namespace GML {
     struct TDATA_COL : public std::vector<TDATA<T>> {
       using std::vector<TDATA<T>>::vector;
 
-      auto col_size() const {
+      size_t col_size() const {
         return (*this)[0].size();
       }
 
-      std::unordered_map<std::string, size_t> count() const {
-        std::unordered_map<std::string, size_t> data_counts{0};
+      CLASS_COUNT count() const {
+        CLASS_COUNT data_counts{0};
         for(const auto tdata : *this) {
           const std::string& name = tdata.label;
           data_counts[name] += 1;
@@ -79,7 +82,6 @@ namespace GML {
         out << tdcol.back() << ")";
         return out;
       }
-
     };
 
   template<typename T>
@@ -116,10 +118,12 @@ namespace GML {
         }
     };
 
+/*
   // IMPURITY DOES NOT HAVE DEFAULT VALUES!
   template<typename T>
     class DECISION_NODE {
       public:
+        std::shared_ptr<TDATA_COL<T>> tdatacol_sptr;
         TDATA_COL<T> tdatacol;
         double impurity;
         QUESTION<T> question;
@@ -159,6 +163,60 @@ namespace GML {
           return out;
         }
     };
+*/
+
+template<typename T> struct NODE_DATA;
+
+  // IMPURITY DOES NOT HAVE DEFAULT VALUES!
+  template<typename T>
+    class DECISION_NODE {
+      private: 
+        std::shared_ptr<TDATA_COL<T>> tdatacol_sptr;
+        std::shared_ptr<DECISION_NODE> true_branch;
+        std::shared_ptr<DECISION_NODE> false_branch;
+
+      public:
+        double impurity;
+        QUESTION<T> question;
+        DECISION_NODE() : tdatacol_sptr{nullptr}, impurity{0.0}, true_branch{nullptr}, false_branch{nullptr} {}
+        DECISION_NODE(
+            std::shared_ptr<TDATA_COL<T>> tdc, 
+            double gini, 
+            QUESTION<T> q, 
+            std::shared_ptr<DECISION_NODE> tbranch = nullptr,
+            std::shared_ptr<DECISION_NODE> fbranch = nullptr
+            ) : tdatacol_sptr{tdc}, impurity{gini}, question{q}, true_branch{tbranch}, false_branch{fbranch} {}
+
+        bool is_leaf() const {
+          return (true_branch == nullptr) && (false_branch == nullptr);
+        }
+
+        friend std::ostream& operator<<(std::ostream& out, const DECISION_NODE& dnode) {
+          out << "DECISION_NODE(" 
+            << *dnode.tdatacol_sptr
+            << ", "
+            << dnode.question 
+            << ", " 
+            << dnode.impurity 
+            << ", " ;
+
+          if(dnode.true_branch == nullptr)
+            out << "NULL";
+          else
+            out << *(dnode.true_branch);
+
+          out << ", ";
+
+          if(dnode.false_branch == nullptr)
+            out << "NULL";
+          else
+            out << *(dnode.false_branch);
+
+          out << ")";
+          return out;
+        }
+    };
+
 
   template<typename T>
     class TREE {
@@ -183,6 +241,7 @@ namespace GML {
           return _find_best_answer(data, *_dtree);
         }
 
+        /*
         DECISION_NODE<T> true_branch() const {
           return *_dtree->true_branch;
         }
@@ -190,6 +249,7 @@ namespace GML {
         DECISION_NODE<T> false_branch() const {
           return *_dtree->false_branch;
         }
+        */
 
         friend std::ostream& operator<<(std::ostream& out, const TREE& tree) {
           out << tree._dtree;
@@ -278,6 +338,83 @@ namespace GML {
     TREE<T>::TREE(TDATA_COL<T>& training_data) : _training_data{training_data} {
       this->_dtree = this->_build_tree(training_data);
     }
+
+  template<typename T> struct NODE_DATA {
+    std::shared_ptr<TDATA_COL<T>> tdatacol_sptr;
+    std::shared_ptr<CLASS_COUNT> count_sptr;
+    std::shared_ptr<PRES_CONFIDENCE> confidence_sptr;
+    double impurity;
+
+    NODE_DATA() : impurity{0.0}, tdatacol_sptr{nullptr}, count_sptr{nullptr}, confidence_sptr{nullptr} {}
+    NODE_DATA(
+        double gini,
+        std::shared_ptr<TDATA_COL<T>> tdc_sptr = nullptr,
+        std::shared_ptr<CLASS_COUNT> cnt_sptr = nullptr,
+        std::shared_ptr<PRES_CONFIDENCE> cnf_sptr = nullptr
+        ) : impurity{gini}, tdatacol_sptr{tdc_sptr}, count_sptr{cnt_sptr}, confidence_sptr{cnf_sptr} {}
+
+    NODE_DATA(const NODE_DATA &nodedata) : 
+      impurity{nodedata.impurity}, 
+      tdatacol_sptr{nodedata.tdatacol_sptr},
+      count_sptr{nodedata.count_sptr},
+      confidence_sptr{nodedata.confidence_sptr} 
+    {
+      std::cout << "NODE_DATA COPY CONSTRUCTED" << std::endl; // DELETE LATER
+    }
+
+    NODE_DATA(NODE_DATA &&nodedata) : 
+      impurity{nodedata.impurity}, 
+      tdatacol_sptr{std::move(nodedata.tdatacol_sptr)},
+      count_sptr{std::move(nodedata.count_sptr)},
+      confidence_sptr{std::move(nodedata.confidence_sptr)} 
+    {
+      std::cout << "NODE_DATA MOVE CONSTRUCTED" << std::endl; // DELETE LATER
+      nodedata.impurity = 0.0;
+    }
+
+    void operator=(const NODE_DATA& nodedata) {
+      std::cout << "NODE_DATA COPY ASSIGNED" << std::endl;
+      impurity = nodedata.impurity;      
+      tdatacol_sptr = nodedata.tdatacol_sptr;
+      count_sptr = nodedata.count_sptr;
+      confidence_sptr = nodedata.confidence_sptr;
+    }
+
+    void operator=(NODE_DATA &&nodedata) {
+      std::cout << "NODE_DATA MOVE ASSIGNED" << std::endl;
+      impurity = nodedata.impurity;
+      tdatacol_sptr = std::move(nodedata.tdatacol_sptr);
+      count_sptr = std::move(nodedata.count_sptr);
+      confidence_sptr = std::move(nodedata.confidence_sptr);
+      nodedata.impurity = 0.0;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const NODE_DATA& nodedata) {
+      out << "NODE_DATA(" << nodedata.impurity << ", ";
+      
+      if(nodedata.tdatacol_sptr)
+        out << *nodedata.tdatacol_sptr;
+      else
+        out << "nullptr";
+
+      out << ", ";
+
+      if(nodedata.count_sptr)
+        out << nodedata.count_sptr;
+      else
+        out << "nullptr";
+
+      out << ", ";
+
+      if(nodedata.confidence_sptr)
+        out << nodedata.confidence_sptr;
+      else
+        out << "nullptr";
+
+      out << ")";
+      return out;
+    }
+  };
 }
 
 using namespace std::literals::string_literals;
@@ -301,8 +438,12 @@ int main() {
       {"Blueberry", {}},
       });
 
-  GML::TREE<std::string> tree(training_data);
-  std::cout << tree.false_branch();
-
+  GML::NODE_DATA<std::string> nodedata{GML::gini(training_data), std::make_shared<GML::TDATA_COL<std::string>>(training_data)};
+  GML::NODE_DATA<std::string> nodedata2;
+  nodedata2 = std::move(nodedata);
+  std::cout << nodedata << std::endl;
+  std::cout << nodedata2.tdatacol_sptr.use_count() << std::endl;
+  std::cout << nodedata.tdatacol_sptr.use_count() << std::endl;
+  
   return 0;
 }
